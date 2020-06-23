6bc2a38da7ea6a099267c78706f5754c83a3308d
Phong Vu <phong.vu@me.com>
2020-06-23 17:41:21 +0700

    changes

---
+++ esphome/components/http.h
@@ -0,0 +1,20 @@
+class MyHandler: public AsyncWebHandler {
+public:
+	MyHandler() {}
+	virtual ~MyHandler() {}
+
+	bool canHandle(AsyncWebServerRequest *request){
+		//request->addInterestingHeader("ANY");
+		return true;
+	}
+
+	void handleRequest(AsyncWebServerRequest *request) {
+		AsyncResponseStream *response = request->beginResponseStream("text/html");
+		response->print("<!DOCTYPE html><html><head><title>Captive Portal</title></head><body>");
+		response->print("<p>This is out captive portal front page.</p>");
+		response->printf("<p>You were trying to reach: http://%s%s</p>", request->host().c_str(), request->url().c_str());
+		response->printf("<p>Try opening <a href='http://%s'>this link</a> instead</p>", WiFi.softAPIP().toString().c_str());
+		response->print("</body></html>");
+		request->send(response);
+	}
+};
